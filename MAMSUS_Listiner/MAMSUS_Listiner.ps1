13e4cd253f289116c13bb94ebefb019d2e1b98da
alrifaii <123451790+alrifaii@users.noreply.github.com>
2024-04-09 13:37:25 +0200

    Update MAMSUS_Listiner.bat

--- MAMSUS_Listiner/MAMSUS_Listiner.ps1
+++ MAMSUS_Listiner/MAMSUS_Listiner.ps1
@@ -1,2 +1,6 @@
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-. '.\mlis.ps1'
+@echo off
+:: Set execution policy to bypass for the current process
+PowerShell -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass"
+
+:: Run the PowerShell script
+PowerShell -File ".\mlis.ps1"
